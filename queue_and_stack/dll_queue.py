846a068f2d7dd62ea6d834ef73872cb6c2c53730
Niyazova <zniyazov@NYMIT058770.fios-router.home>
2020-01-11 08:55:54 -0500

    Add queue and stack solution

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,40 @@
 import sys
 sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import DoublyLinkedList, ListNode
+
+# add to tail remove from head -> DLL
 
 
 class Queue:
     def __init__(self):
-        self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        return self.storage.add_to_tail(ListNode(value))
 
     def dequeue(self):
-        pass
-
+        return self.storage.remove_from_head()
+        
     def len(self):
-        pass
+        return self.storage.length
+
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def enqueue(self, item):
+#         self.size += 1
+#         self.storage.append(item)
+    
+#     def dequeue(self):
+#         if self.size > 0:
+#             self.size -= 1
+#             return self.storage.pop(0)
+#         return None
+
+#     def len(self):
+#         return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,17 +2,37 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+#  add to tail, remove from tail -> array
+
+
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = []
 
     def push(self, value):
-        pass
+        self.size += 1
+        self.storage.append(value)
 
     def pop(self):
-        pass
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.pop()
+        return None
 
     def len(self):
-        pass
+        return self.size
+
+# class Stack:
+#     def __init__(self):
+#         self.storage = DoublyLinkedList()
+
+#     def push(self, value):
+#         self.storage.add_to_tail(ListNode(value))
+
+#     def pop(self):
+#         return self.storage.remove_from_tail()
+
+#     def len(self):
+#         return self.storage.length
